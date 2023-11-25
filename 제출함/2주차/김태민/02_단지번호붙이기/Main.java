78366ce8aabb81fc34568c24f8d04f3e17db615d
TaemHam <tk4003950@gmail.com>
2023-11-26 08:37:25 +0900

    [2주차/02] 김태민 - 설명 추가

--- 제출함/2주차/김태민/02_단지번호붙이기/Main.java
+++ 제출함/2주차/김태민/02_단지번호붙이기/Main.java
@@ -3,19 +3,41 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 /**
+ * 문제 접근 방법:
+ *      지도에 1로 표시된 집들 중 상하좌우로 연결된 집들을 하나로 묶어 단지라고 했을 때,
+ *      단지의 갯수와 각 단지의 크기를 오름차순으로 출력하는 문제.
+ *
+ *      단순히 이중 for문으로 각 칸마다 상하좌우만 보고, 단지 번호가 있는 칸이 있을 때 그 번호를 따라 매겨주는 방법으로 하면
+ *      1 0 1
+ *      1 1 1
+ *      위와 같은 단지는 같은 번호로 묶어줄 수 없다.
+ *
+ *      따라서 이중 for문으로 탐색하다, 단지로 묶이지 않은 집을 보면,
+ *      BFS나 DFS로 주변의 모든 집을 하나의 단지로 묶는 방식으로 진행해야 한다.
+ *
+ *      단지 갯수를 저장할 int, 단지 크기를 저장할 List<Integer>를 초기화하고,
+ *      단지를 하나 탐색할 때마다 갯수에 +1, 크기 리스트에 크기를 추가하고,
+ *      출력 전에 크기 리스트를 정렬해 출력하면 된다.
+ *
+ * 시간 복잡도:
+ *      BFS와 DFS 모두 O(정점의 갯수 + 간선의 갯수) 라는 시간복잡도를 가지는데,
+ *      이번 문제와 같은 2차원 행렬 탐색의 경우,
+ *      정점의 갯수: 2차원 행렬의 칸 갯수 (정확히는 방문 배열의 칸 갯수) = N * N
+ *      간선의 갯수: 모든 칸에서 탐색해야할 상하좌우 방향 = N * N * 4
+ *      이므로, (N * N) + (N * N + 4) 가 나오는데, 상수항 제거하고 영향력 큰 쪽만 남기면
+ *
+ *      O(N * N) or O(N ** 2)
  */
 
 class Coordinate {
 
-    public static int[] rowDirection = new int[] {0, 1, 0, -1};
-    public static int[] colDirection = new int[] {1, 0, -1, 0};
+    public static int[] rowDirection = new int[]{0, 1, 0, -1};
+    public static int[] colDirection = new int[]{1, 0, -1, 0};
 
     public int row;
     public int col;
