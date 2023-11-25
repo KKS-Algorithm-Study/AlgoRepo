06654df75a3854033d7c5750033efd1476cb0ea4
TaemHam <tk4003950@gmail.com>
2023-11-26 08:37:17 +0900

    [2주차/01] 김태민 - 설명 추가

--- 제출함/2주차/김태민/01_DFS와 BFS/Main.java
+++ 제출함/2주차/김태민/01_DFS와 BFS/Main.java
@@ -7,13 +7,26 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringJoiner;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
+ * 문제 접근 방법:
+ *      주어진 정점과 간선을 인접 리스트로 저장 및 정렬 후, BFS와 DFS를 돌려 방문한 순서대로 출력해주는 문제.
+ *
+ * 주의해야할 점:
+ *      BFS는 Queue 자료구조를 이용해 돌리면 되지만,
+ *      DFS는 Stack을 사용하는 방법은 까다롭고, 재귀를 이용해야 쉽게 풀린다.
+ *
+ *      ex) 1번 예제의 경우, BFS를 위해 오름차순으로 정렬한 인접 리스트를 Stack을 사용한 DFS에서 그대로 사용하면
+ *          스택에 [2, 3, 4] 순서로 들어가기 때문에 출력은 거꾸로 4, 3, 2 순서로 나와 틀리게 된다.
+ * 시간 복잡도:
+ *      BFS 및 DFS 둘 다 같다.
+ *      모든 정점을 돌면서(N), 정점에 연결된 간선을 두 번씩(양방향 그래프) 탐색(2 * M)하게 된다.
+ *      따라서 BFS 및 DFS의 시간 복잡도는 관례적으로 다음과 같이 표현한다.
+ *      O(N + M)
  */
 
 public class Main {
@@ -46,7 +59,7 @@ public class Main {
 
     private static void doDfs(List<List<Integer>> graph, int curNode, boolean[] visited, StringJoiner sj) {
         sj.add(Integer.toString(curNode));
-        for (int nxtNode: graph.get(curNode)) {
+        for (int nxtNode : graph.get(curNode)) {
             if (visited[nxtNode]) {
                 continue;
             }
@@ -66,11 +79,11 @@ public class Main {
     private static void doBfs(List<List<Integer>> graph, int initNode, boolean[] visited, StringJoiner sj) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(initNode);
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int curNode = queue.poll();
             sj.add(Integer.toString(curNode));
-            for (int nxtNode: graph.get(curNode)) {
-                if(visited[nxtNode]) {
+            for (int nxtNode : graph.get(curNode)) {
+                if (visited[nxtNode]) {
                     continue;
                 }
                 visited[nxtNode] = true;
