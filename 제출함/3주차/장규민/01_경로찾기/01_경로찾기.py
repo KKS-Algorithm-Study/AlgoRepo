769783ebf8b01439d7a7fb16a5ba3ce14983e590
jgm0327 <gmjang6349@gmail.com>
2023-11-27 23:35:25 +0900

    [3주차/01] 장규민 - 첫 제출

---
+++ 제출함/3주차/장규민/01_경로찾기/01_경로찾기.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+board = [list(input().split()) for _ in range(n)]
+
+max_value = int(1e6)
+
+path = [[1 if board[i][j] == '1' else max_value for j in range(n)] for i in range(n)]
+
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            path[i][j] = min(path[i][j], (path[i][k] + path[k][j]))
+
+for i in range(n):
+    for j in range(n):
+        print(1 if path[i][j] != max_value else 0, end=' ')
+    print()
+
