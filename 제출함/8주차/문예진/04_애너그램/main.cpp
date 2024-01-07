b42d903dd54f98d80349e040b1e1724863fff320
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2024-01-07 21:45:05 +0900

    [8주차/04] 문예진 - 첫 제출

---
+++ 제출함/8주차/문예진/04_애너그램/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        sort(s.begin(), s.end());
+        do {
+            cout << s << '\n';
+        } while (next_permutation(s.begin(), s.end()));
+    }
+}
