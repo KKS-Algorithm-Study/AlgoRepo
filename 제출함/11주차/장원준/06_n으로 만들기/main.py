af180c517900c82853be6bdcc3649767aaebda7e
JANG-WON-JUN <jangwonjun0509@gmail.com>
2024-01-27 18:12:46 +0900

    [11주차/06] 장원준 - 띄어쓰기 수정

--- 제출함/11주차/장원준/06_n으로 만들기/main.py
+++ 제출함/11주차/장원준/06_n으로 만들기/main.py
@@ -4,17 +4,18 @@
 
 2. 시간 복잡도
   1) 최대 문자열 길이만큼 탐색하므로 O(N)이다.
+
 """
 def dfs(string):
   global cnt
   if len(n) == 1:
-    cnt +=1
+    cnt += 1
     return
 
   L = set(list(string))
   # 111 깉이 1개의 숫자로만 이루어진 경우는 만들 수 있는 경우가 1이기 때문이다.
   if len(L) == 1:
-    cnt+=1
+    cnt += 1
     return
   else:
     dfs(string[1:])
