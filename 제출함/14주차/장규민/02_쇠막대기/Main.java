d55e2a1a20bf3aaf18d3a28ef6c9e74a4c73f09b
jgm0327 <gmjang6349@gmail.com>
2024-03-25 12:12:28 +0900

    [14주차/02] 장규민 - 첫 제출

---
+++ 제출함/14주차/장규민/02_쇠막대기/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String sticks = br.readLine();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        int answer = 0;
+        char prev = '(';
+
+        for(int i=0 ; i<sticks.length() ; i++){
+            char ch = sticks.charAt(i);
+            if(ch == '('){
+                stack.add(ch);
+            }
+            else{
+                stack.pollLast();
+                if(prev == '('){
+                    answer += stack.size();
+                }else{
+                    answer++;
+                }
+            }
+
+            prev = ch;
+        }
+
+        System.out.println(answer);
+    }    
+}
